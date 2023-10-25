24c1d0851c4f9b86c0188064fa7614b4a098b4bf
Tage <05jota@skola.engelholm.se>
2023-10-25 14:22:10 +0200

    aa

--- www/public/index.php
+++ www/public/index.php
@@ -1,58 +1,59 @@
 <!doctype html>
 <html lang="sv">
+
 <head>
-	<meta charset="utf-8">
-	<title>Länka in sidor med PHP</title>
-	<link href="css/styleSheet.css" rel="stylesheet" type="text/css">
+   <meta charset="utf-8">
+   <title>Länka in sidor med PHP</title>
+   <link href="css/styleSheet.css" rel="stylesheet" type="text/css">
 </head>
 
 <body>
-	<div id="wrapper">
-		<header>
-      		<h1>Webbserverprogrammering 1 med Bygren</h1>
-      	</header>
-		<!-- header -->
+   <div id="wrapper">
+      <header>
+         <h1>Webbserverprogrammering 1 med Bygren</h1>
+      </header>
+      <!-- header -->
 
-		<section id="leftColumn">
-			<nav>
-	        	<h1>Innehåll</h1>
-				<ul>
-					<li><a href="index.php">Hem</a></li>
-					<li><a href="pages/blogg.php">Blogg</a></li>
-					<li><a href="pages/bilder.php">Bilder</a></li>
-					<li><a href="pages/Kontakt.php">Kontakt</a></li>
-				</ul>
-	        </nav>
-			<aside>
-				<h1>Namn</h1>
-				<p>
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-				Nulla tristique elementum nisl, et vehicula neque laoreet ut. 
-				Praesent sed ultrices purus.
-				</p>
-		  	</aside>
-		</section>
-		<!-- End leftColumn -->
+      <section id="leftColumn">
+         <nav>
+            <h1>Innehåll</h1>
+            <ul>
+               <li><a href="index.php">Hem</a></li>
+               <li><a href="pages/blogg.php">Blogg</a></li>
+               <li><a href="pages/bilder.php">Bilder</a></li>
+               <li><a href="pages/Kontakt.php">Kontakt</a></li>
+            </ul>
+         </nav>
+         <aside>
+            <h1>Namn</h1>
+            <p>
+               Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+               Nulla tristique elementum nisl, et vehicula neque laoreet ut.
+               Praesent sed ultrices purus.
+            </p>
+         </aside>
+      </section>
+      <!-- End leftColumn -->
 
-		<main role="main">
-		<section>
-			<hgroup>
-				<h1>Välkommen till Webbserverprogrammering 1</h1>
-				<h2>Moment 3</h2>
-			</hgroup>
+      <main role="main">
+         <section>
+            <hgroup>
+               <h1>Välkommen till Webbserverprogrammering 1</h1>
+               <h2>Moment 3</h2>
+            </hgroup>
 
-			<p>Min sida</p>     
-	  	</section>
-		</main>
-		<!-- End main -->
+            <p>Min sida</p>
+         </section>
+      </main>
+      <!-- End main -->
 
-		<footer>
-        	<span id="footerLeft">© Sidlayoutarna</span>
-			<span id="footerRight"><a href="mailto:xxx@doman">xxx@doman</a></span>
+      <footer>
+         <span id="footerLeft">© Sidlayoutarna</span>
+         <span id="footerRight"><a href="mailto:xxx@doman">xxx@doman</a></span>
       </footer>
-		<!-- End footer -->
-	</div>
-	<!-- End wrapper -->
+      <!-- End footer -->
+   </div>
+   <!-- End wrapper -->
 </body>
-</html>
 
+</html>
--- www/public/pages/blogg.php
+++ www/public/pages/blogg.php
@@ -1,60 +1,62 @@
 <!doctype html>
 <html lang="sv">
+
 <head>
-	<meta charset="utf-8">
-	<title>Länka in sidor med PHP</title>
-	<link href="css/styleSheet.css" rel="stylesheet" type="text/css">
+    <meta charset="utf-8">
+    <title>Länka in sidor med PHP</title>
+    <link href="css/styleSheet.css" rel="stylesheet" type="text/css">
 </head>
 
 <body>
-	<div id="wrapper">
-		<header>
-      		<h1>Webbserverprogrammering 1 med Bygren</h1>
-      	</header>
-		<!-- header -->
+    <div id="wrapper">
+        <header>
+            <h1>Webbserverprogrammering 1 med Bygren</h1>
+        </header>
+        <!-- header -->
 
-		<section id="leftColumn">
-			<nav>
-	        	<h1>Innehåll</h1>
-				<ul>
-					<li><a href="index.php">Hem</a></li>
-					<li><a href="blogg.php">Blogg</a></li>
-					<li><a href="bilder.php">Bilder</a></li>
-					<li><a href="Kontakt.php">Kontakt</a></li>
-				</ul>
-	        </nav>
-			<aside>
-				<h1>Namn</h1>
-				<p>
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-				Nulla tristique elementum nisl, et vehicula neque laoreet ut. 
-				Praesent sed ultrices purus.
-				</p>
-		  	</aside>
-		</section>
-		<!-- End leftColumn -->
+        <section id="leftColumn">
+            <nav>
+                <h1>Innehåll</h1>
+                <ul>
+                    <li><a href="index.php">Hem</a></li>
+                    <li><a href="blogg.php">Blogg</a></li>
+                    <li><a href="bilder.php">Bilder</a></li>
+                    <li><a href="Kontakt.php">Kontakt</a></li>
+                </ul>
+            </nav>
+            <aside>
+                <h1>Namn</h1>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Nulla tristique elementum nisl, et vehicula neque laoreet ut.
+                    Praesent sed ultrices purus.
+                </p>
+            </aside>
+        </section>
+        <!-- End leftColumn -->
 
-		<main role="main">
-		<section>
-			<hgroup>
-				<h1>Min blogg Nu</h1>
-				<h2>I dag gör vi mallar</h2>
-			</hgroup>
-			<p>Lorem ipsum id duo autem qualisque, cu laoreet eleifend cum. Eu sea 
-					vidit altera civibus, harum deserunt argumentum ne vim. Eu paulo 
-					corpora appareat pro, brute everti iuvaret ne vis. Diam incorrupte 
-					voluptatibus cu vel, nam congue soluta praesent at.
-			</p>    
-	  	</section>
-		</main>
-		<!-- End main -->
+        <main role="main">
+            <section>
+                <hgroup>
+                    <h1>Min blogg Nu</h1>
+                    <h2>I dag gör vi mallar</h2>
+                </hgroup>
+                <p>Lorem ipsum id duo autem qualisque, cu laoreet eleifend cum. Eu sea
+                    vidit altera civibus, harum deserunt argumentum ne vim. Eu paulo
+                    corpora appareat pro, brute everti iuvaret ne vis. Diam incorrupte
+                    voluptatibus cu vel, nam congue soluta praesent at.
+                </p>
+            </section>
+        </main>
+        <!-- End main -->
 
-		<footer>
-        	<span id="footerLeft">© Sidlayoutarna</span>
-			<span id="footerRight"><a href="mailto:xxx@doman">xxx@doman</a></span>
-      </footer>
-		<!-- End footer -->
-	</div>
-	<!-- End wrapper -->
+        <footer>
+            <span id="footerLeft">© Sidlayoutarna</span>
+            <span id="footerRight"><a href="mailto:xxx@doman">xxx@doman</a></span>
+        </footer>
+        <!-- End footer -->
+    </div>
+    <!-- End wrapper -->
 </body>
+
 </html>
